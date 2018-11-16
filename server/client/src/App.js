60779467fbcc57ff32edd9a3fcb4fecd05610a1c
drew23c <andrewcaldwell@ac.c4q.nyc>
2018-11-16 09:20:03 -0500

    added react-bootstrapand styled the home page

--- server/client/src/App.js
+++ server/client/src/App.js
@@ -1,20 +1,37 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {Link, Route, Switch} from 'react-router-dom';
+import {Route, Switch} from 'react-router-dom';
 import Home from './components/Home';
 import Fitness from './components/Fitness';
 import Reviews from './components/Reviews';
+import {Navbar, NavItem, Nav} from 'react-bootstrap';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <nav>
-          <Link to="/">Home</Link>{" "}
-          <Link to="/locations">Fitness</Link>{" "}
-          <Link to="/reviews">Reviews</Link>{" "}
-        </nav>
+        <Navbar inverse collapseOnSelect>
+          <Navbar.Header>
+            <Navbar.Brand>
+              <a href="/">Find Your Gym</a>
+            </Navbar.Brand>
+            <Navbar.Toggle />
+          </Navbar.Header>
+          <Navbar.Collapse>
+            <Nav>
+              <NavItem eventKey={1} href="/">
+                Home
+              </NavItem>
+              <NavItem eventKey={2} href="/locations">
+                Fitness
+              </NavItem>
+              <NavItem eventKey={3} href="/reviews">
+                Reviews
+              </NavItem>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
         <Switch>
           <Route exact path = "/" component={Home} />
           <Route path = "/locations" component={Fitness} />
--- server/client/src/components/Home.js
+++ server/client/src/components/Home.js
@@ -1,21 +1,22 @@
 import React from 'react';
 import SimpleMap from './Map';
 import './styles/home.css';
+import {Jumbotron} from 'react-bootstrap';
 
 const Home = () =>{
     return(
         <div className="home">
-            <h1>Fitness Finder</h1>
+            <Jumbotron>
+                <h1>Find Your Gym!</h1>
+                <p>
+                {`Fitness Finder is an app that features a list of open martial
+                arts gyms sorted from highest to lowest rated by their customers.
+                You can view the reviews from the top rated gyms, get information, 
+                and rate each gym.`}
+                </p>
+            </Jumbotron>
             <div>
                 <iframe width="1000" height="400" src="https://www.youtube.com/embed/ifQHURWXkWY?autoplay=1&amp;controls=0&amp;showinfo=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-            </div>
-            <div>
-                <p>
-                    {`Fitness Finder is an app that features a list of open martial
-                    arts gyms sorted from highest to lowest rated by their customers.
-                    You can view the reviews from the top rated gyms, get information, 
-                    and rate each gym.`}
-                </p>
             </div>
             <div className="simple-map">
                 <SimpleMap/>
