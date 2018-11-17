9762249e439a2df097c3fdb9ed440d0302936ab0
drew23c <andrewcaldwell@ac.c4q.nyc>
2018-11-17 18:13:41 -0500

    widened the intro vid on the home page

--- server/client/src/components/Home.js
+++ server/client/src/components/Home.js
@@ -15,8 +15,8 @@ const Home = () =>{
                 and rate each gym.`}
                 </p>
             </Jumbotron>
-            <div>
-                <iframe width="1000" height="400" src="https://www.youtube.com/embed/ifQHURWXkWY?autoplay=1&amp;controls=0&amp;showinfo=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <div className="map">
+                <iframe width="100%" height="400" src="https://www.youtube.com/embed/ifQHURWXkWY?autoplay=1&amp;controls=0&amp;showinfo=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
             </div>
             <div className="simple-map">
                 <SimpleMap/>
